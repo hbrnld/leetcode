465b3dbefec39bc6ef13e643ba81661694e59df1
Hugo Brunlid <brunlid@kth.se>
2024-01-18 19:56:16 +0100

    Forgot to save

--- Array&String/443. String Compression.py
+++ Array&String/443. String Compression.py
@@ -1,55 +1,33 @@
 # https://leetcode.com/problems/string-compression/?envType=study-plan-v2&envId=leetcode-75
 
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        i = 0
 
-# chars = ["a","a","b","b","c","c","c"]
-# out = []
-# i = 0
+        while i <= len(chars) - 1:
+            count = 1
+            j = 1
 
-# while i <= len(chars)-1: 
-#     out.append(chars[i])
+            # Iterate until no more same chars
+            while i+j <= len(chars) - 1 and chars[i] == chars[i+j]:
+                count += 1
+                j += 1
 
-#     count = 1
-#     while i+1 <= len(chars)-1 and chars[i] == chars[i+1]:
-#         count += 1
-#         i += 1
-    
-#     if count >= 10: 
-#         for digit in str(count):
-#             out.append(digit)
-#     elif count != 1:
-#         out.append(str(count))
-    
-#     i += 1
+            # Handle insertion depending on count
+            if count == 1: 
+                i += 1
+            elif count < 10:
+                chars[i+1] = str(count)
+                del chars[i+2:i+j]
+                i += 2
+            else: 
+                dts = list(str(count))
+                chars[i+1:i+len(dts)] = dts
+                del chars[i+len(dts)+1:i+j+1]
+                i += len(dts) + 1
 
-# chars = out
-
-# print(chars)
-# print(out)
-#return len(out)
-
-chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
-i = 0
-
-while i <= len(chars) - 1:
-    count = 1
-    j = 1
-
-    while i+j <= len(chars) - 1 and chars[i] == chars[i+j]:
-        count += 1
-        j += 1
-
-    if count == 1: 
-        i += 1
-    elif count < 10:
-        chars[i+1] = str(count)
-        del chars[i+2:i+j]
-        i += 2
-    else: # More or equal to 10
-        dts = list(str(count))
-        print(chars)
-        print(dts)
-        chars[i+1:i+len(dts)] = dts
-        del chars[i+len(dts)+1:i+j+1]
-        i += len(dts) + 1
-
-print(chars)
+        return len(chars)
--- Two Pointers/1679. Max Number of K-sum Pairs.py
+++ Two Pointers/1679. Max Number of K-sum Pairs.py
@@ -1,23 +1,45 @@
 # https://leetcode.com/problems/max-number-of-k-sum-pairs/?envType=study-plan-v2&envId=leetcode-75
 
-nums = [3,1,3,4,3]
-k = 6
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
 
+        nums.sort()
+        ops, left, right = 0, 0, len(nums)-1
 
-nums.sort()
-ops = 0
-left = 0
-right = len(nums)-1            
+        while left < right: 
+            s = nums[left] + nums[right] 
+            if s < k:
+                left += 1 
+            elif s > k: 
+                right -= 1
+            else: 
+                ops += 1
+                left += 1
+                right -= 1
+        return ops
 
-while left < right: 
-    s = nums[left] + nums[right] 
-    if s < k:
-        left += 1 
-    elif s > k: 
-        right -= 1
-    else: 
-        ops += 1
-        left += 1
-        right -= 1
+    def maxOperationsCounter(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        from Collections import Counter
 
-print(ops)
+        ops, counter = 0, Counter(nums)
+        
+        for value, cnt in counter.items(): 
+            diff = k - value     # Find matching value that sums to k
+
+            if diff in counter and diff <= value: 
+                if value != diff:
+                    ops += min(cnt, counter[diff])
+                else: 
+                    ops += cnt // 2
+        
+        return ops
